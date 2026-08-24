85c7e5380cd37ba49314772fef4c8e77a0be41a4
Saude Muriae site <souzajefinny@gmail.com>
2026-08-24 13:51:25 -0300

    git

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,10 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis;
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
